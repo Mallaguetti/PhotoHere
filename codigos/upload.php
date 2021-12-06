7e21374d1fc4a37762de117658d1e1b9224666fd
Gabriel Mallaguetti <gabrielmallaguetti2@gmail.com>
2021-12-05 21:52:48 -0300

    upload de image

--- codigos/upload.php
+++ codigos/upload.php
@@ -11,8 +11,14 @@
         $extencao = end($extencao);
 
         if (in_array($extencao,$permitidos)){
-            $sql = "INSERT INTO foto VALUES(default, '$nomes[$i]', $idEnsaio)";
+            date_default_timezone_set('America/Bahia');
+            $novoNome = $idEnsaio."_".date('d-m-y-H-i-s')."_".$nomes[$i];
+
+            $sql = "INSERT INTO foto VALUES(default, '$novoNome', $idEnsaio)";
             mysqli_query($conexao, $sql) or die (mysqli_error($conexao));
+
+            $mover = move_uploaded_file($_FILES['arquivos']['tmp_name'][$i], '../fotos/' . $novoNome);
+
             header("Location:../perfilFotografoEnviarFotos.php?idEnsaio=$idEnsaio&msg=Arquivos enviados com sucesso");
         } else {
             header("Location:../perfilFotografoEnviarFotos.php?msg=Alguns arquivos não foram enviados por Extencao de arquivo não permitida");
