f2910ba51857f59a1c9645500ef0945329920924
Gabriel Mallaguetti <gabrielmallaguetti2@gmail.com>
2021-12-12 20:51:58 -0300

    Update encontrarFotografoPerfil.php

--- encontrarFotografoPerfil.php
+++ encontrarFotografoPerfil.php
@@ -11,6 +11,10 @@
         $instagram = $registro["instagram"];
         $facebook = $registro["facebook"];
         $celular = $registro["celular"];
+
+        $foto = $registro["fotoPerfil"];
+        $fotoPerfil = base64_encode($foto);
+
     } else {
         header("Location:encontrarFotografo.php");
     }
@@ -23,7 +27,7 @@
     <link rel="shortcut icon" href="imagens/LOGO.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="estilos/_principal.css">
     <link rel="stylesheet" type="text/css" href="estilos/perfil.css">
-    <link rel="stylesheet" type="text/css" href="estilos/individuais/perfilCliente.css">
+    <link rel="stylesheet" type="text/css" href="estilos/verPerfil.css">
 </head>
 <body>
     <header>
@@ -31,26 +35,47 @@
         <?php require_once "codigos/HTMLcabecalho.php";?>
         </nav>
     </header>
-    <section class="flex topo"id="s1">
-        <div>
-            <p>nuemro: <?php echo $celular?></p>
-            <p>email: <?php echo $email?></p>
-            <p>facebook: <?php echo $facebook?></p>
-            <p>insta: <?php echo $instagram?></p>
-        </div>
-        <div>
-        <?php
+    <section class="flex topo"id="s1" >
+        <div id="dados">
+            <?php
+            if ($foto != null) {
+                echo ("<img style='max-width: 30vw; max-height:30vh; margin: auto;' src='data:imagem/IMG_JPG;base64,$fotoPerfil'>");
+            } else {
+                echo ("<img src='imagens/perfil.jpg'>");
+            }
+            if(isset($celular)){
+                if ($celular != ""){
+                    echo("<p>Nuemro: $celular</p>");
+                }
+            };
+            if(isset($email)){
+                if ($email != ""){
+                    echo("<p>Email: $email</p>");
+                }
+            };
+            if(isset($facebook)){
+                if ($facebook != ""){
+                    echo("<p>Facebook: $facebook</p>");
+                }
+            };
+            if(isset($instagram)){
+                if ($instagram != ""){
+                    echo("<p>Instagram: $instagram</p>");
+                }
+            };
             require_once "codigos/validarSessao.php";
             if ($sessaoExiste) {
                 if(!$isFotografo){
-                    echo "<a href='editarEnsaio.php?id=$id&editar=false'>Marcar Ensaio</a>";
+                    echo "<br><a href='editarEnsaio.php?id=$id&editar=false'>Marcar Ensaio</a>";
                 } else {
-                    echo "Apenas clientes podem marcar ensaio";
+                    echo "<br>Apenas clientes podem marcar ensaio";
                 };
             } else{
-                echo "Faça login para marcar ensaio";
+                echo "<br>Faça login para marcar ensaio";
             };
-        ?>
+            ?>
+            <br>
+            <a href="encontrarFotografo.php">Voltar</a>
         </div>
     </section>
 </body>
